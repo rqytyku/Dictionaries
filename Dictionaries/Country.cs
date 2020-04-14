0aef3d577173b1fad51020f31cc8ed4bda1d3dd5
Romela Qytyku <rqytyku2@uet.edu.al>
2020-04-14 12:20:39 +0200

    Add project files.

---
+++ Dictionaries/Country.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionaries
+{
+    public  class Country
+    {
+        public string name { get; }
+        public string code { get; }
+        public string region { get; }
+        public int population { get; }
+       
+
+        public Country(string name, string code, string region, int population)
+        {
+            this.name = name;
+            this.code = code;
+            this.region= region;
+            this.population = population;
+            
+        }
+    }
+}
---
+++ Dictionaries/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Country albania = new Country("Albania", "AL", "Europe", 2_867_000);
+            Country norway = new Country("Norway", "NOR", "Europe", 5_368_000);
+
+            //Dictionary<string Country > countries = new Dictionary<string, Country>();
+            var countries= new Dictionary<string, Country>();
+            countries.Add(albania.code, albania);
+            countries.Add(norway.code, norway);
+
+            //Country selectCountry = countries["AL"];
+            //Console.WriteLine(selectCountry.name);
+            foreach (var country in countries.Values)
+                Console.WriteLine(country.name);
+            Console.ReadKey();
+        }
+    }
+}
